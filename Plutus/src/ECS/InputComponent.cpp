377266a8281697d522b89ef61afbd0cff4cc10ba
royel21 <royelconsoro@hotmail.com>
2020-12-14 16:37:39 -0400

    return some changes

--- Plutus/src/ECS/InputComponent.cpp
+++ Plutus/src/ECS/InputComponent.cpp
@@ -17,27 +17,23 @@ namespace Plutus
     void InputComponent::Update(float deltaTime)
     {
         m_aninComp->setAnimate(false);
-        if (m_input->onKeyDown(SDLK_UP) ||
-            (m_gamepad->isConnected(0) && m_gamepad->isBtnDown(0, GAMEPAD_UP) || m_gamepad->getLstickY(0) > 150))
+        if (m_input->onKeyDown(SDLK_UP) || m_gamepad->isBtnDown(0, GAMEPAD_UP) || (m_gamepad->isConnected(0) && m_gamepad->getLstickY(0) > 150))
         {
             m_transfComp->position.y += 5;
             m_aninComp->PlayAnimation("walk-up", true);
         }
-        if (m_input->onKeyDown(SDLK_DOWN) ||
-            (m_gamepad->isConnected(0) && (m_gamepad->isBtnDown(0, GAMEPAD_DOWN) || m_gamepad->getLstickY(0) < 110)))
+        if (m_input->onKeyDown(SDLK_DOWN) || m_gamepad->isBtnDown(0, GAMEPAD_DOWN) || (m_gamepad->isConnected(0) && m_gamepad->getLstickY(0) < 110))
         {
             m_transfComp->position.y -= 5;
             m_aninComp->PlayAnimation("walk-down", true);
         }
-        if (m_input->onKeyDown(SDLK_RIGHT) ||
-            (m_gamepad->isConnected(0) && m_gamepad->isBtnDown(0, GAMEPAD_RIGHT) || m_gamepad->getLstickX(0) > 150))
+        if (m_input->onKeyDown(SDLK_RIGHT) || m_gamepad->isBtnDown(0, GAMEPAD_RIGHT) || (m_gamepad->isConnected(0) && m_gamepad->getLstickX(0) > 150))
         {
             m_transfComp->position.x += 5;
             m_imgComp->setFlipX(false);
             m_aninComp->PlayAnimation("run", true);
         }
-        if (m_input->onKeyDown(SDLK_LEFT) ||
-            (m_gamepad->isConnected(0) && m_gamepad->isBtnDown(0, GAMEPAD_LEFT) || m_gamepad->getLstickX(0) < 110))
+        if (m_input->onKeyDown(SDLK_LEFT) || m_gamepad->isBtnDown(0, GAMEPAD_LEFT) || (m_gamepad->isConnected(0) && m_gamepad->getLstickX(0) < 110))
         {
             m_transfComp->position.x -= 5;
             m_imgComp->setFlipX(true);
